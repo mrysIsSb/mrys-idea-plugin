8969b7a2914c91933465c03635a01a0a8cb0f8a3
mrys <1510218499@qq.com>
2024-09-21 21:44:52 +0800

    文档添加关于字符串、字节和void数据类型的说明
    在NumPy文档中，常规介绍部分现在包含了关于字符串、字节和void数据类型的信息，以及现有的数值类型。

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
+//        javaCompiler("21")
+//        local(providers.gradleProperty("localPath"))
         create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
